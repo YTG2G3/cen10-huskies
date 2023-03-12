3107c6aeb8ce668b4c851362c65b47b1447ede20
YTG2G3 <scratchlover05@gmail.com>
2023-03-12 04:03:32 -0700

    added calendar

--- screens/events-screen.jsx
+++ screens/events-screen.jsx
@@ -1,12 +1,26 @@
+import { useContext } from "react";
 import { SafeAreaView } from "react-native";
-import { Text } from "react-native-paper";
+import { Calendar } from "react-native-calendars";
+import SiteContext from "../lib/site-context";
+import styles from '../styles/events.scss';
 
 export default function EventsScreen() {
-
+    let { dark } = useContext(SiteContext);
 
     return (
-        <SafeAreaView>
-
+        <SafeAreaView style={styles.container}>
+            <Calendar
+                key={dark}
+                theme={{
+                    todayTextColor: "#F24343",
+                    arrowColor: "#F24343",
+                    monthTextColor: "#F24343",
+                    calendarBackground: "transparent",
+                    dayTextColor: dark ? "#FFFFFF" : "#3C1111",
+                    textDisabledColor: dark ? "#2d4150" : "#d9e1e8",
+                    'stylesheet.day.basic': { 'base': { height: 100 } }
+                }}
+            />
         </SafeAreaView>
     );
 }
