d222fe3ade909cb411ffff0cbd1b0217343947b7
YTG2G3 <scratchlover05@gmail.com>
2023-03-12 05:19:22 -0700

    completed modal for calendar

--- screens/events-screen.jsx
+++ screens/events-screen.jsx
@@ -1,16 +1,19 @@
 import { format } from "date-fns";
 import { getDocs } from "firebase/firestore";
 import { useContext, useEffect, useState } from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, View } from "react-native";
 import { Calendar } from "react-native-calendars";
+import { Modal, Portal, Text } from "react-native-paper";
 import { fevent } from "../lib/firebase";
 import SiteContext from "../lib/site-context";
 import styles from '../styles/events.scss';
 
 export default function EventsScreen() {
     let { dark } = useContext(SiteContext);
-    let [marks, setMarks] = useState([]);
+    let [marks, setMarks] = useState({});
+    let [events, setEvents] = useState({});
     let [rerender, setRerender] = useState(0);
+    let [selDay, setSelDay] = useState(undefined);
 
     useEffect(() => { loadData() }, []);
 
@@ -20,23 +23,24 @@ export default function EventsScreen() {
     // Load current events
     const loadData = async () => {
         let m = await getDocs(fevent);
-        setMarks(m.docs);
-    }
-
-    // Render the events
-    const renderDots = (marks) => {
         let tmp = {};
+        let tt = {}
 
-        for (let ev of marks) {
+        for (let ev of m.docs) {
             let e = ev.data();
 
             let d = format(e.date.toDate(), "yyyy-MM-dd");
-            let p = tmp[d] ? [...tmp[d].dots] : []
+            let p = tmp[d] ? [...tmp[d].dots] : [];
 
-            tmp[d] = { dots: [...p, { key: ev.id, color: e.important ? "#F24343" : "#FAB9B9" }] }
+            // Important stuff first
+            let dots = e.important ? [{ key: ev.id, color: e.important ? "#F24343" : "#FAB9B9" }, ...p] : [...p, { key: ev.id, color: e.important ? "#F24343" : "#FAB9B9" }];
+
+            tmp[d] = { dots, selected: e.important ? true : tmp[d]?.selected ?? false };
+            tt[ev.id] = e;
         }
 
-        return tmp;
+        setMarks(tmp);
+        setEvents(tt);
     }
 
     return (
@@ -44,18 +48,36 @@ export default function EventsScreen() {
             <Calendar
                 key={rerender}
                 markingType="multi-dot"
-                markedDates={renderDots(marks)}
+                markedDates={marks}
                 theme={{
-                    todayTextColor: "#F24343",
+                    todayTextColor: "#00adf5",
                     arrowColor: "#F24343",
                     monthTextColor: "#F24343",
                     calendarBackground: "transparent",
                     dayTextColor: dark ? "#FFFFFF" : "#3C1111",
                     textDisabledColor: dark ? "#2d4150" : "#d9e1e8",
+                    selectedDayTextColor: "#F24343",
+                    selectedDayBackgroundColor: "transparent",
+                    textDayFontSize: 22,
+                    textMonthFontSize: 22,
+                    textDayHeaderFontSize: 14,
                     'stylesheet.day.basic': { 'base': { height: 100 } }
                 }}
-                onDayPress={(day) => { }}
+                onDayPress={setSelDay}
             />
+
+            <Portal>
+                <Modal contentContainerStyle={styles.modal} visible={selDay} onDismiss={() => setSelDay(undefined)}>
+                    {selDay ? <Text style={styles.cen}>{selDay.dateString}</Text> : undefined}
+                    {selDay ? marks[selDay.dateString].dots.map((v, i) => (
+                        <View style={{ marginBottom: i < marks[selDay.dateString].dots.length - 1 ? 20 : 0, alignSelf: 'flex-start' }} key={i}>
+
+                            <Text style={{ color: events[v.key].important ? "red" : "black", alignSelf: 'flex-start' }}>#{i + 1}: {events[v.key].name} {events[v.key].important ? "(IMPORTANT!)" : undefined}</Text>
+                            <Text style={{ alignSelf: 'flex-start' }}>{events[v.key].description}</Text>
+                        </View>
+                    )) : undefined}
+                </Modal>
+            </Portal>
         </SafeAreaView>
     );
 }
