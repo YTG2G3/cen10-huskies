aabb764df2eff6cf15aa33c67d14888042ab9ed6
YTG2G3 <scratchlover05@gmail.com>
2023-03-14 20:45:57 -0700

    upcoming events

--- screens/events-screen.jsx
+++ screens/events-screen.jsx
@@ -1,11 +1,12 @@
-import { format } from "date-fns";
+import { add, format } from "date-fns";
 import { getDocs } from "firebase/firestore";
 import { useContext, useEffect, useState } from "react";
-import { SafeAreaView, View } from "react-native";
+import { SafeAreaView, ScrollView, View } from "react-native";
 import { Calendar } from "react-native-calendars";
-import { Modal, Portal, Text } from "react-native-paper";
+import { List, Modal, Portal, Surface, Text } from "react-native-paper";
 import { fevent } from "../lib/firebase";
 import SiteContext from "../lib/site-context";
+import { lightTheme } from "../lib/theme";
 import styles from '../styles/events.scss';
 
 export default function EventsScreen() {
@@ -45,35 +46,44 @@ export default function EventsScreen() {
 
     return (
         <SafeAreaView style={styles.container}>
-            <Calendar
-                key={rerender}
-                markingType="multi-dot"
-                markedDates={marks}
-                theme={{
-                    todayTextColor: "#00adf5",
-                    arrowColor: "#F24343",
-                    monthTextColor: "#F24343",
-                    calendarBackground: "transparent",
-                    dayTextColor: dark ? "#FFFFFF" : "#3C1111",
-                    textDisabledColor: dark ? "#2d4150" : "#d9e1e8",
-                    selectedDayTextColor: "#F24343",
-                    selectedDayBackgroundColor: "transparent",
-                    textDayFontSize: 22,
-                    textMonthFontSize: 22,
-                    textDayHeaderFontSize: 14,
-                    'stylesheet.day.basic': { 'base': { height: 100 } }
-                }}
-                onDayPress={setSelDay}
-            />
+            <View>
+                <Calendar
+                    key={rerender}
+                    markingType="multi-dot"
+                    markedDates={marks}
+                    theme={{
+                        todayTextColor: "#00adf5",
+                        arrowColor: "#F24343",
+                        monthTextColor: "#F24343",
+                        calendarBackground: "transparent",
+                        dayTextColor: dark ? "#FFFFFF" : "#3C1111",
+                        textDisabledColor: dark ? "#2d4150" : "#d9e1e8",
+                        selectedDayTextColor: "#F24343",
+                        selectedDayBackgroundColor: "transparent",
+                        textDayFontSize: 22,
+                        textMonthFontSize: 22,
+                        textDayHeaderFontSize: 14,
+                        'stylesheet.day.basic': { 'base': { height: 100 } }
+                    }}
+                    onDayPress={setSelDay}
+                />
+
+                <List.Section title="Upcoming events">
+                    {new Array(3).fill(0).map((x, i) => (
+                        <List.Accordion id={i + ""} key={i} title={format(add(new Date(), { days: i }), "yyyy/MM/dd")}>
+                            {marks[format(add(new Date(), { days: i }), "yyyy-MM-dd")]?.dots.map((v, ii) => <List.Item key={ii} title={events[v.key]?.name} description={events[v.key]?.description} left={() => <List.Icon color={events[v.key]?.important ? "#F24343" : undefined} icon={events[v.key]?.important ? "exclamation" : "pound"} />} />)}
+                        </List.Accordion>
+                    ))}
+                </List.Section>
+            </View>
 
             <Portal>
                 <Modal contentContainerStyle={styles.modal} visible={selDay} onDismiss={() => setSelDay(undefined)}>
-                    {selDay ? <Text style={styles.cen}>{selDay.dateString}</Text> : undefined}
+                    {selDay ? <Text theme={lightTheme} style={styles.cen}>{selDay.dateString}</Text> : undefined}
                     {selDay ? marks[selDay.dateString].dots.map((v, i) => (
                         <View style={{ marginBottom: i < marks[selDay.dateString].dots.length - 1 ? 20 : 0, alignSelf: 'flex-start' }} key={i}>
-
-                            <Text style={{ color: events[v.key].important ? "red" : "black", alignSelf: 'flex-start' }}>#{i + 1}: {events[v.key].name} {events[v.key].important ? "(IMPORTANT!)" : undefined}</Text>
-                            <Text style={{ alignSelf: 'flex-start' }}>{events[v.key].description}</Text>
+                            <Text theme={lightTheme} style={{ color: events[v.key].important ? "red" : "black", alignSelf: 'flex-start' }}>#{i + 1}: {events[v.key].name} {events[v.key].important ? "(IMPORTANT!)" : undefined}</Text>
+                            <Text theme={lightTheme} style={{ alignSelf: 'flex-start' }}>{events[v.key].description}</Text>
                         </View>
                     )) : undefined}
                 </Modal>
