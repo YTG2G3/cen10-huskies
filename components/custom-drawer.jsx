a5e145a5f4c457153b6bd6097f726e4985778d07
TheAsianFish <jmchung2006@gmail.com>
2023-03-16 00:09:56 -0700

    Translated custom drawer page

--- components/custom-drawer.jsx
+++ components/custom-drawer.jsx
@@ -2,6 +2,7 @@ import { DrawerContentScrollView, DrawerItemList } from "@react-navigation/drawe
 import { signOut } from "firebase/auth";
 import { addDoc } from "firebase/firestore";
 import { useContext, useState } from "react";
+import { useTranslation } from "react-i18next";
 import { View } from "react-native";
 import { Button, Dialog, Portal, Snackbar, TextInput } from "react-native-paper";
 import { fauth, freport } from "../lib/firebase";
@@ -9,6 +10,7 @@ import SiteContext from "../lib/site-context";
 import styles from '../styles/custom-drawer.scss';
 
 export default function CustomDrawer(props) {
+    const { t } = useTranslation();
     let { setUser } = useContext(SiteContext);
     let [rep, setRep] = useState(false);
     let [inc, setInc] = useState("");
@@ -40,24 +42,24 @@ export default function CustomDrawer(props) {
                 <DrawerItemList {...props} />
             </View>
 
-            <Button style={styles.btn} mode="contained" icon="bug" onPress={() => setRep(true)}>Bug Report</Button>
-            <Button style={styles.btn} mode="contained" icon="logout" onPress={logout}>Logout</Button>
+            <Button style={styles.btn} mode="contained" icon="bug" onPress={() => setRep(true)}>{t("BugReport")}</Button>
+            <Button style={styles.btn} mode="contained" icon="logout" onPress={logout}>{t("Logout")}</Button>
 
             <Portal>
                 <Dialog style={styles.diag} visible={rep} onDismiss={() => setRep(false)}>
-                    <Dialog.Title>Report a bug</Dialog.Title>
+                    <Dialog.Title>{t("ReportText")}</Dialog.Title>
 
                     <Dialog.Content>
-                        <TextInput value={inc} onChangeText={setInc} label="Inconvenience" />
+                        <TextInput value={inc} onChangeText={setInc} label={t("Inconvenience")} />
                     </Dialog.Content>
 
                     <Dialog.Actions>
-                        <Button onPress={reportBug}>Submit</Button>
-                        <Button onPress={resetDiag}>Cancel</Button>
+                        <Button onPress={reportBug}>{t("Submit")}</Button>
+                        <Button onPress={resetDiag}>{t("Cancel")}</Button>
                     </Dialog.Actions>
                 </Dialog>
 
-                <Snackbar visible={snk} onDismiss={() => setSnk(false)}>Reported! Thank you for your cooperation.</Snackbar>
+                <Snackbar visible={snk} onDismiss={() => setSnk(false)}>{t("BugText")}</Snackbar>
             </Portal>
         </DrawerContentScrollView>
     );
